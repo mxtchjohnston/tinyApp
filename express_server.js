4925a67d246346fc226ee98cce2fe2ed7fe0bcf2
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-02 22:39:17 +0000

    added urls template

--- express_server.js
+++ express_server.js
@@ -9,18 +9,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+const routes = {
+  '/': function(req, res) {
+    res.send("hello");
+  },
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  '/urls.json': function(req, res) {
+    res.json(urlDatabase);
+  },
 
-});
+  'urls': function(req, res) {
+    const templateVars = {urls: urlDatabase};
+    res.render("urls_index", templateVars);
+  },
+  
+  '/hello': function(req, res) {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  },
+}
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+for (const r in routes) {
+  app.get(r, routes[r]);
+  console.log(r);
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
