d4e975b583711daa5c75843a3e05af8a913edfb3
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-03 00:14:11 +0000

    added post logic

--- express_server.js
+++ express_server.js
@@ -3,6 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomString = function(num) {
+  const randomNum = () => Math.floor(Math.random() * 94) + 32;
+  const array = [];
+  for (let i = 0; i < num; i++) {
+    array.push(randomNum());
+  }
+  return String.fromCodePoint(...array);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,16 +37,28 @@ const routes = {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   },
 
+  '/urls/new': function(req, res) {
+    res.render("urls_new");
+  },
+
   '/urls/:id': function(req, res) {
     const vars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
     res.render('url_show', vars);
   }
-}
+};
 
 for (const r in routes) {
   app.get(r, routes[r]);
-  console.log(r);
+  //console.log(r);
 }
+
+app.post("/urls", (req, res) => {
+  //console.log(req.body); // Log the POST request body to the console
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString(6)] = req.body.longURL;
+  res.redirect(200, '/urls');
+  res.end();
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
